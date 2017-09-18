784f53cdfc0e717cefa48a90d9a599d3dc5b59f8
William Lee <william.lee@dominos.com.au>
2017-09-18 19:48:53 +1000

    Added getCompletedAuditsCount to Audit Service

--- src/app/services/audit.service.ts
+++ src/app/services/audit.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Http } from "@angular/http";
+import { Http, RequestOptions, Headers } from "@angular/http";
 
 @Injectable()
 export class AuditService implements OnInit {
-  
+  private url:string = "http://incontrolpty.australiaeast.cloudapp.azure.com:7123/webservices/api/CategoryCompletedAudits"
+
   constructor(private http:Http) { }
    ngOnInit(): void {
     throw new Error("Method not implemented.");
+  }
+  getCompletedAuditsCount(startMilis: Number, endMillis: Number) {
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let requestData = { "UserId": 1, "StartMillis": 2, "EndMillis": 3 };
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.url, requestData, options);
   }
 }
