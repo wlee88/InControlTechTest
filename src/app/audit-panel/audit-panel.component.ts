b462b5260a35b24ed8dfd08f31246633ae34b0c0
William Lee <william.lee@dominos.com.au>
2017-09-19 12:47:13 +1000

    UI changes throughout

--- src/app/audit-panel/audit-panel.component.ts
+++ src/app/audit-panel/audit-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'audit-panel',
   templateUrl: './audit-panel.component.html',
-  styleUrls: ['./audit-panel.component.css']
+  styleUrls: ['./audit-panel.component.scss']
 })
 export class AuditPanelComponent implements OnInit {
   @Input() figure:string= "0";
